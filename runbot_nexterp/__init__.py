8bf9c75d803b6ef1618acf069f75417006b0c347
root <root@runbot.nexterp.ro>
2021-02-19 12:36:05 +0000

    antother try contollers

--- runbot_nexterp/__init__.py
+++ runbot_nexterp/__init__.py
@@ -1,4 +1,4 @@
 from . import models
 from . import container
-from . import contollers
+from . import controllers
   # not imported because we are puting the mailhog from build.py runbot.build.parms
